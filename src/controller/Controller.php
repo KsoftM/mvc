29ee270b8b0306789921aa21b325659dbe89527e
KsoftM <ksoftmanagement@gmail.com>
2021-10-09 19:49:21 +0530

    stable relece

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -2,7 +2,79 @@
 
 namespace ksoftm\system\controller;
 
+use ReflectionClass;
+use ksoftm\system\DB;
+use ReflectionMethod;
+use ksoftm\system\model\Model;
+use ksoftm\system\kernel\Route;
+use ksoftm\system\utils\html\BuildMixer;
 
 class Controller
 {
+    private static ?string $resourcesPath = null;
+    private static array $landDirectory = [];
+
+    /**
+     * Class constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    // TODO controller must be configure
+    public static function config(string $resourcesPath, array $landDirectory): void
+    {
+        self::$resourcesPath = $resourcesPath;
+        self::$landDirectory = $landDirectory;
+    }
+
+    /**
+     * specified model for the pedicular controller with user routing data
+     *
+     * @return \ksoftm\system\model\Model|false
+     */
+    public function GetModel(): Model|false
+    {
+        $model = trim(
+            str_replace(
+                pathinfo(Controller::class, PATHINFO_FILENAME),
+                '',
+                pathinfo($this::class, PATHINFO_FILENAME)
+            )
+        );
+
+        $c = new ReflectionClass($model);
+        $c = $c->getName();
+        $model = new $c();
+        if ($model instanceof Model) {
+            $id = $model->getPrimaryKey();
+            if (!empty($id)) {
+                $data = Route::resolve()->getUserPathData();
+
+                if (is_array($data) && array_key_exists($id, $data)) {
+                    $data = $data[$id];
+
+                    $model->findAndLoad([$data]);
+                    $model->$id = $data;
+
+                    return $model;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function view(string $path, array $data = []): bool
+    {
+        // get the rout path
+        BuildMixer::build(
+            self::$resourcesPath,
+            self::$resourcesPath . "/$path",
+            self::$landDirectory,
+            $data,
+        );
+
+        return false;
+    }
 }
--- src/model/Model.php
+++ src/model/Model.php
@@ -2,31 +2,312 @@
 
 namespace ksoftm\system\model;
 
-use ksoftm\system\utils\ArgumentsTrait;
+use Exception;
+use ksoftm\system\DB;
+use ksoftm\system\database\Query;
+use ksoftm\system\internal\DResult;
+use ksoftm\system\utils\validator\MegaValid;
+use ksoftm\system\utils\datatype\Dictionary;
+use ksoftm\system\utils\validator\MegRule;
 
-class Model
+abstract class Model
 {
-    use ArgumentsTrait;
+    /** @var ListData $args arguments datas. */
+    public Dictionary $args;
 
-    final public function __get(string $name)
+
+
+    /**
+     * primary key of the columns to load and find the data
+     *
+     * @var array
+     */
+    protected array $primaryKeys = [];
+
+    /**
+     * unique key of the columns to load and find the data
+     *
+     * @var array
+     */
+    protected array $uniqueKeys = [];
+
+    /**
+     * all important fields for the query to fill
+     *
+     * @var array
+     */
+    protected array $fillable = [];
+
+    /**
+     * given data field for public access
+     *
+     * @var array
+     */
+    protected array $graded = [];
+
+    /**
+     * hidden field for the public access
+     *
+     * @var array
+     */
+    protected array $hidden = [];
+
+    /**
+     * Class constructor.
+     *
+     * @param array|null $data
+     */
+    public function __construct()
     {
-        if (key_exists($name, $this->getAll())) {
-            return $this->getArguments($name, 1);
-        }
+        $this->args = new Dictionary();
+    }
 
+    /**
+     * public getter
+     *
+     * @param string $name
+     *
+     * @return mixed
+     */
+    public function __get(string $name): mixed
+    {
+        if ($this->args->haveKey($name)) {
+            return $this->args->getValue($name);
+        }
         return null;
     }
 
-    final public function __set(string $name, string $data)
+
+    /**
+     * public setter
+     *
+     * @param string $name
+     * @param string $data
+     */
+    public function __set(string $name, string $data)
     {
-        $this->setArguments($name, $data, 1, true);
+        $this->args->add($name, $data);
     }
 
-    public function findById(string $id, string $colum = 'id'): void
+    public abstract function rules(): array;
+
+
+    protected function validate(): array
     {
+        $rules = [];
+
+        $this->fillable = [];
+
+        array_map(function (MegRule $val) use (&$rules) {
+            // assign the fillable field with name
+            $field = $this->fillable[] = $val->getField(false);;
+
+            // generate the rules for tha field and values
+            $rules[] = [$this->args->getValue($field), $val];
+        }, $this->rules());
+
+        // validate the rules and return the result and can get the processed error messages
+        $validate = MegaValid::validate($rules, $errors);
+
+        return [$validate, $errors];
     }
 
-    private function tableName(): string
+    public function getErrors(): array
+    {
+        return $this->validate()[1];
+    }
+
+    public function isValid(): bool
+    {
+        return $this->validate()[0];
+    }
+
+    /**
+     * find and load data using primary key or unique key you specified
+     *
+     * @param array $data
+     *
+     * @return boolean
+     */
+    public function findAndLoad(array $data): bool
+    {
+        /**
+         * @var DResult
+         */
+        $result = $this->findByPrimaryKey($data);
+
+        if ($result == false) {
+            /**
+             * @var DResult
+             */
+            $result = $this->findByUniqueKeys($data);
+        }
+
+        if ($result instanceof DResult && $result->rowCount() === 1) {
+            return $this->loadData($result);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * find data by primary keys and load if needed
+     *
+     * @param string $id
+     * @param string $column
+     *
+     * @return void
+     */
+    protected function findByPrimaryKey(array $keys): DResult|false
+    {
+        return $this->find($this->primaryKeys, $keys);
+    }
+
+    /**
+     * find data by unique keys and load if needed
+     *
+     * @param string $id
+     * @param string $column
+     *
+     * @return void
+     */
+    protected function findByUniqueKeys(array $keys): DResult|false
+    {
+        return $this->find($this->uniqueKeys, $keys);
+    }
+
+    /**
+     * load data using DResult
+     *
+     * @param \ksoftm\system\internal\DResult $result
+     *
+     * @return boolean
+     */
+    protected function loadData(DResult $result): bool
+    {
+        if ($result->rowCount() == 1) {
+            foreach ($result->getData()[0] as $key => $value) {
+                $this->$key = $value;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function find(array $fields, array $data): DResult|false
+    {
+        if (!empty($fields) && !empty($data)) {
+            $result = DB::select(
+                $this->tableName(),
+                fn (Query $q) => $q->where(
+                    $this->conditionField($fields),
+                    $data
+                )
+            );
+            return $result;
+        }
+        return false;
+    }
+
+    public function getPrimaryKey(): ?string
+    {
+        return $this->primaryKeys[0] ?? null;
+    }
+
+    private function conditionField(array $fields): string
+    {
+        $condition = [];
+        foreach ($fields as $key) {
+            $condition[] = "$key = ?";
+        }
+
+        return implode(' and ', $condition);
+    }
+
+    private function conditionData(array $fields): array
+    {
+        $data = [];
+        foreach ($fields as $key) {
+            $data[] = $this->args->getValue($key);
+        }
+
+        return $data;
+    }
+
+    public function insert(): DResult|false
+    {
+        $set = [];
+
+        array_map(function (string $key) use (&$set) {
+            $set[$key] = $this->args->getValue($key);
+        }, $this->fillable);
+
+        if ($this->isValid() == true) {
+            return DB::insert(
+                $this->tableName(),
+                fn (Query $q) => $q->field($this->fillable)->set($set)
+            );
+        }
+
+        return false;
+    }
+
+    public function update(): DResult|false
+    {
+        $set = [];
+
+        array_map(function (string $key) use (&$set) {
+            $set[$key] = $this->args->getValue($key);
+        }, $this->fillable);
+
+        if ($this->isValid() == true) {
+            return DB::delete(
+                $this->tableName(),
+                fn (Query $q) => $q
+                    ->set($set)
+                    ->where(
+                        $this->conditionField($this->primaryKeys),
+                        $this->conditionData($this->primaryKeys)
+                    )
+            );
+        }
+
+        return false;
+    }
+    public function delete(): DResult|false
+    {
+        $set = [];
+
+        $this->args->getEach(function ($key, $val) use (&$set) {
+            $set[$key] = $val[1];
+        });
+
+        if ($this->isValid() == true) {
+            return DB::update(
+                $this->tableName(),
+                fn (Query $q) => $q
+                    ->field($this->fillable)
+                    ->set($set)
+                    ->where(
+                        $this->conditionField($this->primaryKeys),
+                        $this->conditionData($this->primaryKeys)
+                    )
+            );
+        }
+
+        return false;
+    }
+
+    public function first(int $count = 1, int $offset = 0): DResult|false
+    {
+        return DB::select(
+            $this->tableName(),
+            fn (Query $q) => $q->limit($count, $offset)
+        );
+    }
+
+    protected function tableName(): string
     {
         return strtolower(pathinfo($this::class, PATHINFO_FILENAME) . 's');
     }
