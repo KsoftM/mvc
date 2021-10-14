8e64122ac3f29defa67a006b398e16322cabb12e
KsoftM <ksoftmanagement@gmail.com>
2021-10-14 13:47:51 +0530

    view changes

--- src/helper.php
+++ src/helper.php
@@ -1,11 +1,12 @@
 <?php
 
 use ksoftm\system\controller\Controller;
+use ksoftm\system\utils\View;
 
 if (!function_exists('view')) {
     function view(string $path, array $data): void
     {
-        $p = new Controller();
+        $p = new View();
         $p->view($path, $data);
     }
 }
